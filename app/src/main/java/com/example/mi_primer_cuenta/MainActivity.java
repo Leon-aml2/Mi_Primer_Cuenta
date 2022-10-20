479d4cf72a85a751f53e856a45156b23bd7c445f
COP412L <totalb028@gmail.com>
2022-10-20 14:53:22 -0300

    clase 1 Firebase

--- app/src/main/java/com/example/mi_primer_cuenta/MainActivity.java
+++ app/src/main/java/com/example/mi_primer_cuenta/MainActivity.java
@@ -3,12 +3,23 @@ package com.example.mi_primer_cuenta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button btn_Agregar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btn_Agregar = (Button) findViewById(R.id.btn_Agregar);
+        btn_Agregar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hola", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
